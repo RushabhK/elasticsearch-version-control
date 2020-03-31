b19802e7d96d03b7360abaa0d8090f30c04b5a7b
Rushabh Kabra <meetrushabh@gmail.com>
2020-04-01 01:36:47 +0530

    Add methods for index_service and alias_service contracts.

--- service/alias_service.go
+++ service/alias_service.go
@@ -4,7 +4,10 @@ import (
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
-type AliasService interface{}
+type AliasService interface {
+	SetAlias(alias string, index string) error
+	GetIndexVersion(alias string) (int, error)
+}
 
 type aliasService struct {
 	esClient *elasticsearch.Client
@@ -14,4 +17,12 @@ func NewAliasService(esClient *elasticsearch.Client) AliasService {
 	return aliasService{
 		esClient: esClient,
 	}
+}
+
+func (aliasService) SetAlias(alias string, index string) error {
+	panic("implement me")
+}
+
+func (aliasService) GetIndexVersion(alias string) (int, error) {
+	panic("implement me")
 }
--- service/index_service.go
+++ service/index_service.go
@@ -2,7 +2,12 @@ package service
 
 import "github.com/elastic/go-elasticsearch/v7"
 
-type IndexService interface{}
+type IndexService interface {
+	CreateIndex(indexName string, configuration string) error
+	ReIndex(sourceIndex string, targetIndex string, script string) (int, error)
+	DeleteIndex(indexName string) error
+	GetDocumentsCount(indexName string) (int, error)
+}
 
 type indexService struct {
 	esClient *elasticsearch.Client
@@ -12,4 +17,20 @@ func NewIndexService(esClient *elasticsearch.Client) IndexService {
 	return indexService{
 		esClient: esClient,
 	}
+}
+
+func (indexService) CreateIndex(indexName string, configuration string) error {
+	panic("implement me")
+}
+
+func (indexService) ReIndex(sourceIndex string, targetIndex string, script string) (int, error) {
+	panic("implement me")
+}
+
+func (indexService) DeleteIndex(indexName string) error {
+	panic("implement me")
+}
+
+func (indexService) GetDocumentsCount(indexName string) (int, error) {
+	panic("implement me")
 }
