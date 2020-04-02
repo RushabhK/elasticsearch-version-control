b5cca6be9e6914428016c9e7a8052876255034eb
Rushabh Kabra <meetrushabh@gmail.com>
2020-04-02 23:04:10 +0530

    Implement GetNextIndexVersion method of VersionDetailsProvider to give the list of index details of the migrations to be run.

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module elasticsearch-version-control
 go 1.13
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/elastic/go-elasticsearch/v7 v7.5.0
 	github.com/golang/mock v1.4.3
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.5.1
 )
--- helper/version_details_provider.go
+++ helper/version_details_provider.go
@@ -2,6 +2,12 @@ package helper
 
 import (
 	"elasticsearch-version-control/model"
+	"encoding/json"
+	"errors"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"strconv"
 )
 
 type VersionDetailsProvider interface {
@@ -17,5 +23,57 @@ func NewVersionDetailsProvider(pathToMigrations string) VersionDetailsProvider {
 }
 
 func (versionDetailsProvider versionDetailsProvider) GetNextIndexVersions(alias string, currentVersion int) ([]model.IndexDetails, error) {
-	panic("Implement me")
+	pathToAlias := versionDetailsProvider.pathToMigrations + alias
+	pathToIndexConfigs := pathToAlias + "/index_configs/"
+	pathToScripts := pathToAlias + "/scripts/"
+	file, err := os.Open(pathToIndexConfigs)
+	if err != nil {
+		logrus.Error(err.Error())
+		return nil, err
+	}
+	defer file.Close()
+
+	indexConfigFiles, err := file.Readdirnames(0)
+	if err != nil {
+		logrus.Error(err.Error())
+		return nil, err
+	}
+
+	var indexDetails []model.IndexDetails
+
+	for version := currentVersion + 1; true; version += 1 {
+		fileVersion := "v" + strconv.Itoa(version)
+		indexConfigFile := fileVersion + ".json"
+		if !isFilePresent(indexConfigFile, indexConfigFiles) {
+			break
+		}
+		indexConfigBytes, _ := ioutil.ReadFile(pathToIndexConfigs + indexConfigFile)
+		var jsonObj map[string]interface{}
+		if json.Unmarshal(indexConfigBytes, &jsonObj) != nil {
+			errorMsg := pathToIndexConfigs + indexConfigFile + " file is not a valid json"
+			logrus.Error(errorMsg)
+			return nil, errors.New(errorMsg)
+		}
+		indexConfig := string(indexConfigBytes)
+		scriptFile := fileVersion + ".painless"
+		scriptBytes, _ := ioutil.ReadFile(pathToScripts + scriptFile)
+		script := string(scriptBytes)
+		nextIndexDetails := model.IndexDetails{
+			Version:     version,
+			Alias:       alias,
+			IndexConfig: indexConfig,
+			Script:      script,
+		}
+		indexDetails = append(indexDetails, nextIndexDetails)
+	}
+	return indexDetails, nil
+}
+
+func isFilePresent(file string, indexConfigFiles []string) bool {
+	for _, indexConfigFile := range indexConfigFiles {
+		if file == indexConfigFile {
+			return true
+		}
+	}
+	return false
 }
