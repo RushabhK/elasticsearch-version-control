ccd7aeb67dc8fef9b5082aeb0be0fdb4bbb1dd12
Rushabh Kabra <meetrushabh@gmail.com>
2020-04-05 23:59:08 +0530

    Implement SetAlias method in alias_service.

--- service/alias_service.go
+++ service/alias_service.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v7"
+	"net/http"
+	"strings"
 )
 
 type AliasService interface {
@@ -19,8 +23,23 @@ func NewAliasService(esClient *elasticsearch.Client) AliasService {
 	}
 }
 
-func (aliasService) SetAlias(alias string, index string) error {
-	panic("implement me")
+func (aliasService aliasService) SetAlias(alias string, index string) error {
+	requestBody := fmt.Sprintf(`{
+	"actions" : [
+			{ "remove" : { "index" : "*",  "alias" : "%v" } },
+			{ "add" : { "index" : "%v", "alias" : "%v" } }
+		]
+	}`, alias, index, alias)
+	reader := strings.NewReader(requestBody)
+
+	response, aliasError := aliasService.esClient.Indices.UpdateAliases(reader)
+	if aliasError != nil {
+		return aliasError
+	}
+	if response.StatusCode != http.StatusOK {
+		return errors.New("could not set alias")
+	}
+	return nil
 }
 
 func (aliasService) GetIndexVersion(alias string) (int, error) {
